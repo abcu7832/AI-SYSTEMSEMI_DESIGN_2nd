2021ada5c0223855441e1b4bf9c9289edb60ec0d
abcu7832 <abcu7832@gmail.com>
2025-08-18 20:44:30 +0900

    Create RV32I_tb.sv

---
+++ 06_CPU_Design/250818/HOMEWORK/RV32I_tb.sv
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+module RV32I_tb ();
+
+    logic clk;
+    logic reset;
+
+    MCU dut (.*);
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk   = 0;
+        reset = 1;
+        #10 reset = 0;
+        #300 $finish;
+    end
+endmodule
