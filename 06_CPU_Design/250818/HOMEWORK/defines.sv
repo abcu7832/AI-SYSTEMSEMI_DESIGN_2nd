aae89a37d838cf445131927dc23a004666455721
abcu7832 <abcu7832@gmail.com>
2025-08-18 20:44:15 +0900

    Create defines.sv

---
+++ 06_CPU_Design/250818/HOMEWORK/defines.sv
@@ -0,0 +1,25 @@
+`define OP_TYPE_R 7'b0110011
+`define ADD 4'b0000
+`define SUB 4'b1000
+`define SLL 4'b0001
+`define SRL 4'b0101
+`define SRA 4'b1101
+`define SLT 4'b0010
+`define SLTU 4'b0011
+`define XOR 4'b0100
+`define OR 4'b0110
+`define AND 4'b0111
+
+`define OP_TYPE_L 7'b0000011
+`define LB 3'b000
+`define LH 3'b001
+`define LW 3'b010
+`define LBU 3'b100
+`define LHU 3'b101
+
+`define OP_TYPE_I 7'b0010011
+
+`define OP_TYPE_S 7'b0100011
+`define SB 3'b000
+`define SH 3'b001
+`define SW 3'b010
